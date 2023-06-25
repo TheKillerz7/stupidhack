b23153b702309c541a86266ecbc4aeafbc2dca0e
David Shu <mstudym13@gmail.com>
2023-06-25 15:01:23 +0700

    finallll

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -8,7 +8,7 @@ export default function ThankYouPage() {
       <div className="text-3xl animate-shake-slow mb-10">
         Just joking, no payment here hehe😜
       </div>
-      <Link href="/file/stupidcapy.exe">
+      <Link href="/file/StupidGame.zip">
         <div className="lg:block lg:mx-0 lg:mt-4 relative text-lg cursor-pointer bg-[#bc9364] text-white w-max rounded-full py-2 px-20 mx-auto">
           <div className="bg-[#bb9871] w-full h-full rounded-full absolute top-0 left-0 -z-10 blur-xl"></div>
           <div className="absolute bottom-0 right-14 translate-y-4 w-5 h-7 -rotate-12">
